5f07214aa0881fd22a5eba2fa38493cffb9fd172
Artem Borodovoy <a.borodovoi@gmail.com>
2024-04-26 01:52:07 +0300

    added annimation to Button

--- shared/Button/Button.tsx
+++ shared/Button/Button.tsx
@@ -4,15 +4,45 @@ import {
   PressableProps,
   Text,
   View,
+  GestureResponderEvent,
+  Animated,
 } from 'react-native';
 import { Colors, Fonts, Radius } from '../tokens';
 
 export function Button({ text, ...props }: PressableProps & { text: string }) {
+  const animatedValue = new Animated.Value(100);
+  const color = animatedValue.interpolate({
+    inputRange: [0, 100],
+    outputRange: [Colors.primaryHover, Colors.primary],
+  });
+
+  const fadeIn = (e: GestureResponderEvent) => {
+    Animated.timing(animatedValue, {
+      toValue: 0,
+      duration: 100,
+      useNativeDriver: false,
+    }).start();
+    props.onPressIn && props.onPressIn(e);
+  };
+
+  const fadeOut = (e: GestureResponderEvent) => {
+    Animated.timing(animatedValue, {
+      toValue: 100,
+      duration: 100,
+      useNativeDriver: false,
+    }).start();
+    props.onPressOut && props.onPressOut(e);
+  };
+
   return (
-    <Pressable {...props}>
-      <View style={styles.button}>
+    <Pressable {...props} onPressIn={fadeIn} onPressOut={fadeOut}>
+      <Animated.View
+        style={{
+          ...styles.button,
+          backgroundColor: color,
+        }}>
         <Text style={styles.text}>{text}</Text>
-      </View>
+      </Animated.View>
     </Pressable>
   );
 }
@@ -22,7 +52,6 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     alignItems: 'center',
     height: 58,
-    backgroundColor: Colors.primary,
     borderRadius: Radius.r10,
   },
   text: {
