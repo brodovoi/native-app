9c9e41b33997ac8937d5d03550a4dcf255b7267e
Artem Borodovoy <a.borodovoi@gmail.com>
2024-05-01 00:32:37 +0300

    updated layout - addeed SafeArea and Statusbar

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,24 +1,34 @@
-import { Slot, Stack, Tabs } from 'expo-router';
+import { Stack, Tabs } from 'expo-router';
 import { Colors } from '../shared/tokens';
-import { Text } from 'react-native';
+import { StatusBar } from 'expo-status-bar';
+import {
+  SafeAreaProvider,
+  useSafeAreaInsets,
+} from 'react-native-safe-area-context';
 
 export default function RootRayout() {
+  const insets = useSafeAreaInsets();
+
   return (
-    <Stack
-      screenOptions={{
-        statusBarColor: Colors.black,
-        contentStyle: {
-          backgroundColor: Colors.black,
-        },
-        headerShown: false,
-      }}>
-      <Stack.Screen name="login" />
-      <Stack.Screen
-        name="restore"
-        options={{
-          presentation: 'modal',
-        }}
-      />
-    </Stack>
+    <SafeAreaProvider>
+      <StatusBar style="light" />
+      <Stack
+        screenOptions={{
+          statusBarColor: Colors.black,
+          contentStyle: {
+            backgroundColor: Colors.black,
+            paddingTop: insets.top,
+          },
+          headerShown: false,
+        }}>
+        <Stack.Screen name="login" />
+        <Stack.Screen
+          name="restore"
+          options={{
+            presentation: 'modal',
+          }}
+        />
+      </Stack>
+    </SafeAreaProvider>
   );
 }
