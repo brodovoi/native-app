3c2ca7267819f8890dc27a37deed4cd57bb5db73
Artem Borodovoy <a.borodovoi@gmail.com>
2024-04-30 23:34:48 +0300

    added expo router

--- app/index.tsx
+++ app/index.tsx
@@ -7,10 +7,10 @@ import {
   Alert,
   Platform,
 } from 'react-native';
-import { Input } from './shared/Input/Input';
-import { Colors, Gaps } from './shared/tokens';
-import { Button } from './shared/Button/Button';
-import { ErrorNotification } from './shared/ErrorNotification/ErrorNotification';
+import { Input } from '../shared/Input/Input';
+import { Colors, Gaps } from '../shared/tokens';
+import { Button } from '../shared/Button/Button';
+import { ErrorNotification } from '../shared/ErrorNotification/ErrorNotification';
 import { useState } from 'react';
 
 export default function App() {
@@ -30,7 +30,7 @@ export default function App() {
       <View style={styles.content}>
         <Image
           style={styles.logo}
-          source={require('./assets/logo.png')}
+          source={require('../assets/logo.png')}
           resizeMode="contain"
         />
         <View style={styles.form}>
