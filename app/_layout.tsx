ba97c336f5b5dd04c2ca2dc2d74274b61f0fa652
Artem Borodovoy <a.borodovoi@gmail.com>
2024-05-01 00:20:47 +0300

    added stack

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,6 +1,24 @@
 import { Slot, Stack, Tabs } from 'expo-router';
+import { Colors } from '../shared/tokens';
 import { Text } from 'react-native';
 
 export default function RootRayout() {
-  return <Stack />;
+  return (
+    <Stack
+      screenOptions={{
+        statusBarColor: Colors.black,
+        contentStyle: {
+          backgroundColor: Colors.black,
+        },
+        headerShown: false,
+      }}>
+      <Stack.Screen name="login" />
+      <Stack.Screen
+        name="restore"
+        options={{
+          presentation: 'modal',
+        }}
+      />
+    </Stack>
+  );
 }
